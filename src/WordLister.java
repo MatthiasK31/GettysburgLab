21ceec95a47dd5de3c6b2a89099b7eb4c9bc16c0
Matthias Kim <matthiastk805@gmail.com>
2021-10-26 14:50:02 -0400

    Fixed up to number 8 after Ky restarted my laptop

--- src/WordLister.java
+++ src/WordLister.java
@@ -5,49 +5,55 @@ import java.io.*;
 
 
 public class WordLister {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         try {
             //variable declarations
             String word = "";
+            Scanner in = new Scanner(System.in);
             Scanner file = new Scanner(new File("src\\gettysburg.txt"));
-            ArrayList<String> words = new ArrayList();
-            int position = 0;
+            ArrayList<String> words = new ArrayList<>();
 
             //loop through the file and allocate each item to its own spot in the arraylist
             while (file.hasNext()) {
                 words.add(file.next());
             }
             //loop through the arraylist and print each item individually
-            for (int i = 0; i < words.size(); i++) {
-                System.out.println(words.get(i));
+            for (String value : words) {
+                System.out.println(value);
             }
 
+            //can do enhanced for loop to sort
+
             //loop through the entire arraylist and take the average of the lengths
+
             double average = 0;
             int longestLength = words.get(0).length();
             String longestWord = words.get(0);
-            for (int i = 0; i < words.size() - 1; i++) {
+            for (String s : words){
                 StringBuilder temp = new StringBuilder();
-                for (int j = 0; j < words.get(i).length() - 1; j++) {
-                    if (words.get(i).charAt(j) == ',' || words.get(i).charAt(j) == '.' || words.get(i).charAt(j) == '-')
+                for (int i = 0; i < s.length(); i++) {
+                    if (s.charAt(i) == ',' || s.charAt(i) == '.' || s.charAt(i) == '-')
                         continue;
                     else
-                        temp.append(words.get(i).charAt(j));
+                        temp.append(s.charAt(i));
                 }
-                if (words.get(i).length() > longestLength) {
-                    longestLength = words.get(i).length();
-                    longestWord = words.get(i);
+                if (s.length() > longestLength) {
+                    longestLength = s.length();
+                    longestWord = s;
                 }
-
                 average += temp.length();
             }
             average /= words.size();
 
             System.out.println("What word would you like to search for in the Gettysburg Address?");
-            Iterator<String> i = words.iterator();
-            while (i.hasNext()){
-
+            String searchWord = in.nextLine();
+            int timesFound = 0;
+            for (String s : words) {
+                if (s.equalsIgnoreCase(searchWord)) {
+                    timesFound++;
+                }
             }
+            System.out.printf("The word %s was found a total of %d times.\n", searchWord, timesFound);
 
 
             System.out.printf("The average length of a word is %.2f letters.\n", average);
