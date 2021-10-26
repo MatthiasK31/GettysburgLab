052b7297046149774a31d641dbef37d659b7a247
matthiask31 <matthiastk805@gmail.com>
2021-10-25 23:49:08 -0400

    Parts 1-2 Done

--- src/WordLister.java
+++ src/WordLister.java
@@ -4,28 +4,43 @@ import java.io.*;
 
 public class WordLister {
     public static void main(String[] args) throws FileNotFoundException {
+        //variable declarations
         String word = "";
-        Scanner file = new Scanner(new File("src\\fish.txt"));
+        Scanner file = new Scanner(new File("src\\gettysburg.txt"));
         ArrayList<String> words = new ArrayList();
         int position = 0;
-        // assume filename stores the name of the file
+
+        //loop through the file and allocate each item to its own spot in the arraylist
         while (file.hasNext())
         {
-            String temp = "";
             words.add(file.next());
-            for (int i = 0; i < words.get(position).length(); i++) {
-                if (words.get(position).charAt(i) == ',')
-                    continue;
-                else
-                    temp += words.get(position).charAt(i);
-            }
-            words.set(position, temp);
-            position++;
+
+        }
+        //loop through the arraylist and print each item individually
+        for (int i = 0; i < words.size(); i++)
+        {
+            System.out.println(words.get(i));
         }
 
-        System.out.println(words);
-        System.out.println(word);
-        System.out.println("hi");
+        //loop through the entire arraylist and take the average of the lengths
+        double average = 0;
+        for (int i = 0; i < words.size()-1; i++)
+        {
+            StringBuilder temp = new StringBuilder();
+            for (int j = 0; j < words.get(i).length()-1; j++) {
+                if (words.get(i).charAt(j) == ',')
+                    continue;
+                else
+                    temp.append(words.get(j).charAt(j));
+            }
+            average += temp.length();
+        }
+        average /= words.size();
+        System.out.println("The average length of a word is " + average + " letters.");
+
+
+
+
 
     }
 }
